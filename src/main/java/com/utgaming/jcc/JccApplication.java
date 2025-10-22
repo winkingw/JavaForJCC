3ec6ce7453a6257e487696c119356258d8d736d3
takagisan <2428593329@qq.com>
2025-10-22 22:47:16 +0800

    首次提交

---
+++ src/main/java/com/utgaming/jcc/JccApplication.java
@@ -0,0 +1,13 @@
+package com.utgaming.jcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JccApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JccApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/utgaming/jcc/JccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utgaming.jcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JccApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
